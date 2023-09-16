1ba7baad9c59d9ef1b029002cde3f19b86cc6c17
LeoVtr76 <leo.vauthier76@gmail.com>
2023-09-16 23:03:07 +0200

    Add hardhat ts project and create contract

---
+++ client/contracts/Wallet.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Wall {
+    address owner;
+    uint totalBalance;
+    constructor() {
+        owner = msg.sender;
+    }
+    modifier onlyOwner() {
+        require (msg.sender == owner);
+        _;
+    }
+    struct account {
+        uint balance;
+    }
+
+    mapping (address => account) accounts;
+
+    function send() external payable {
+        accounts[msg.sender].balance += msg.value;
+        totalBalance += msg.value;
+    }
+    function getBalance() external view returns(uint) {
+        return accounts[msg.sender].balance;
+    }
+    function withdrawAll() external {
+        uint _balance = accounts[msg.sender].balance;
+        require(_balance != 0);
+        require(_balance <= address(this).balance);
+        payable(msg.sender).transfer(_balance);
+        totalBalance -= _balance;
+    }
+    function withdrawAllAdmin() external onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+        totalBalance -= address(this).balance;
+    }
+    receive() external payable {}
+    fallback() external payable {}
+
+}
