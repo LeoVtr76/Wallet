185da61c051c85c594d09082251ba0af9878c7c3
LeoVtr76 <leo.vauthier76@gmail.com>
2023-09-16 05:09:18 +0200

    add ethereum counter

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,15 @@ import flouImg from './img/flou.png'
 function App() {
   return (
     <div className="App">
-            <div className="wallet">ETHEREUM WALLET</div>
-      <div className="greeting">BONJOUR LEO</div>
-      <div className="logo">
-      <img className="flou noselect" src={flouImg} alt="flou" />
-        <img className="circle noselect" src={circleImg} alt="circle" />
-        <img className="ether noselect" src={etherImg} alt="ether" />
+      <div className="wallet noselect">ETHEREUM WALLET</div>
+      <div className="greeting noselect">BONJOUR LEO</div>
+      <div className="logoContainer">
+        <div className="logo">
+          <img className="flou noselect" src={flouImg} alt="flou" />
+          <img className="circle noselect" src={circleImg} alt="circle" />
+          <img className="ether noselect" src={etherImg} alt="ether" />
+        </div>
+        <div className="ethAmount">100 ETH</div>
       </div>
       <button className="btn-send">DEPOSER DE L'ETHER</button>
       <button className="btn-receive">RECUPERER DE L'ETHER</button>
