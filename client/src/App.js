7b680fa6a918f507ff20954bc78ec749e275b435
LeoVtr76 <leo.vauthier76@gmail.com>
2023-09-16 04:32:36 +0200

    add blur image behind ethereum logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import circleImg from './img/circle.png';
 import etherImg from './img/ether.png';
+import flouImg from './img/flou.png'
 
 function App() {
   return (
     <div className="App">
       <div className="logo">
+      <img className="flou noselect" src={flouImg} alt="flou" />
         <img className="circle noselect" src={circleImg} alt="circle" />
         <img className="ether noselect" src={etherImg} alt="ether" />
       </div>
