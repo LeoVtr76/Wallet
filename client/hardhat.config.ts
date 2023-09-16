03e5c3b7cbb9f2fd070ba83b7f065532f688b5b7
LeoVtr76 <leo.vauthier76@gmail.com>
2023-09-17 00:48:14 +0200

    update hardhat.config.ts

--- client/hardhat.config.ts
+++ client/hardhat.config.ts
@@ -7,12 +7,12 @@ const config: HardhatUserConfig = {
   solidity: "0.8.19",
   paths : {
     artifacts: './src/artifacts' },
-    networks : {
-      maticTest : {
-        url : `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+  networks : {
+    maticTest : {
+      url : `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts : [process.env.WALLET_PRIVATE_KEY||""]
     }
-    }
+  }
 };
 
 export default config;
