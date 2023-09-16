5c5ab20048ce11f4c761cad24434aa07a41fb942
LeoVtr76 <leo.vauthier76@gmail.com>
2023-09-16 22:34:31 +0200

    Add Line under text

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import circleImg from './img/circle.png';
 import etherImg from './img/ether.png';
-import flouImg from './img/flou.png'
+import flouImg from './img/flou.png';
+import leftLineImg from './img/leftLine.png'
+import rightLineImg from './img/rightLine.png'
 
 function App() {
   return (
     <div className="App">
       <div className="wallet noselect">ETHEREUM WALLET</div>
+      <img className="leftLine" src={leftLineImg} alt="line" />
       <div className="greeting noselect">BONJOUR LEO</div>
+      <img className="rightLine" src={rightLineImg} alt="line" />
       <div className="logoContainer">
         <div className="logo">
           <img className="flou noselect" src={flouImg} alt="flou" />
