e9d33091473e95f8660dace00d5ca87dfd8711d3
LeoVtr76 <leo.vauthier76@gmail.com>
2023-09-18 17:51:25 +0200

    Add ganache blockchain in hardhat.config.ts

--- client/hardhat.config.ts
+++ client/hardhat.config.ts
@@ -11,7 +11,14 @@ const config: HardhatUserConfig = {
     maticTest:{
       url : `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts : [process.env.WALLET_PRIVATE_KEY||""]
-    }
+    },
+    ganache: {
+      url: "http://127.0.0.1:7545",
+      chainId: 1337,
+      accounts: [
+        `0xa459ab146902b9fdb29f7702dbc60f9680f9dba10b3cf4c1b0c886945d4fe0c6`,
+      ],
+    },
   }
 };
 
