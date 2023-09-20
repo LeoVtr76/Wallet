a2d0bf77121ace2aabc2da5a3e8d81f596d54b17
LeoVtr76 <leo.vauthier76@gmail.com>
2023-09-20 03:26:48 +0200

    Update App.js and SetName component

--- client/src/App.js
+++ client/src/App.js
@@ -13,74 +13,66 @@ import ErrorPopup from './components/ErrorPopup';
 import SetName from './components/SetName';
 
 const WalletAddress = process.env.REACT_APP_CONTRACT_ADDRESS;
+
 function App() {
   const [state, setState] = useState({contract : null,});
   const [balance, setBalance] = useState();
-  const [showNameInput, setShowNameInput] = useState(false);
-  const [userName, setUserName] = useState('');
   const [finalUserName, setFinalUserName] = useState('');
   const [currentError, setCurrentError] = useState({});
+  const [isMounted, setIsMounted] = useState(true);
+  const [useName , setUseName] = useState();
+  const [currentAccount, setCurrentAccount] = useState();
 
   useEffect(() => {
-    let isMounted = true; 
     const connectWallet = async () => {
       try {
-          const { ethereum } = window;
-          if (!ethereum) {
-            setCurrentError({localCode : "ERR003", localMessage : "MetaMask n'est pas installé!", notClosable : true});
-            return; // Sortez de la fonction si MetaMask n'est pas installé
-          }
-          await ethereum.request({method: "eth_requestAccounts"});
-          const provider = new ethers.BrowserProvider(window.ethereum);
-          const signer = await provider.getSigner();
-          const contract = new ethers.Contract(
-              WalletAddress,
-              Wallet.abi,
-              signer
-          );
-          if(isMounted){
-            setState({ contract });
-            fetchBalance(contract);
-            checkName(contract);
-          }
-      } catch (error) {
+        const { ethereum } = window;
+        if (!ethereum) {
+          setCurrentError({localCode : "ERR003", localMessage : "MetaMask n'est pas installé!", notClosable : true});
+          return; // Sortez de la fonction si MetaMask n'est pas installé
+        }
+        await ethereum.request({method: "eth_requestAccounts"});
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        const signer = await provider.getSigner();
+        const contract = new ethers.Contract(
+            WalletAddress,
+            Wallet.abi,
+            signer
+        );
+        if(isMounted){
+          console.log("isMounted");
+          setState({ contract });
+          fetchBalance(contract);
+          setUseName(true);
+        }
+      }
+      catch (error) {
         console.log("Error in connectWaller" ,error);
       }
     };
     const handleAccountsChanged = async (accounts) => {
-        if (accounts.length === 0) {
-            console.log('L\'utilisateur a déconnecté MetaMask');
-        } else {
-            console.log('Compte sélectionné par l\'utilisateur:', accounts[0]);
-            if (state.contract) {
-                fetchBalance(state.contract);
-                checkName(state.contract);
-            }
-        }
+      if (accounts.length === 0) {
+          console.log('L\'utilisateur a déconnecté MetaMask');
+      } 
+      else {
+          console.log('Compte sélectionné par l\'utilisateur:', accounts[0]);
+          setCurrentAccount(accounts[0]);
+          setIsMounted(true);
+          connectWallet();
+      }
     };
     if (window.ethereum) {
         window.ethereum.on('accountsChanged', handleAccountsChanged);
     }
     connectWallet();
     return () => {
-      isMounted = false;
+      setIsMounted(false);
       if (window.ethereum) {
-          window.ethereum.removeListener('accountsChanged', handleAccountsChanged); // Supprimez l'écouteur
+        window.ethereum.removeListener('accountsChanged', handleAccountsChanged); // Supprimez l'écouteur
       }
     };
-  }, [state.contract]);
+  }, [state.contract, currentAccount]);
 
-  const checkName = async (contract) => {
-    const name = await contract.getName();
-    setFinalUserName(name);
-    if (name === "") {
-      setShowNameInput(true);
-    } 
-    else {
-      setShowNameInput(false);
-      setUserName(name);
-    }
-  }
   const fetchBalance = async (contract) => {
     try {
       const _balance = await contract.getBalance();
@@ -132,7 +124,7 @@ function App() {
           <img className="ether noselect" draggable="false" src={etherImg} alt="ether" />
         </div>
         <div className="ethAmount">{balance === "0.0" ? "0" : balance || 0} ETH</div>
-        <SetName contract={state.contract} onError={setCurrentError} onFinalUsername={setUserName} />
+        <SetName contract={state.contract} onError={setCurrentError} onFinalUsername={setFinalUserName} useName = {useName} setUseName = {setUseName} />
       </div>
       <button onClick={sendEth} className="btn-send noselect">DEPOSER DE L'ETHER</button>
       <button onClick={withdrawAll}className="btn-receive noselect">RECUPERER DE L'ETHER</button>
--- client/src/components/SetName.js
+++ client/src/components/SetName.js
@@ -1,25 +1,30 @@
 import React, { useState, useEffect } from 'react';
 
-function SetName({ contract, onError, onFinalUsername}) {
+function SetName({ contract, onError, onFinalUsername, useName, setUseName}) {
     const [userName, setUserName] = useState('');
     const [showNameInput, setShownameInput] = useState(false);
     useEffect(() => {
-        if (contract){
+        console.log("useEffect used in SetName");
+        if (useName){
             checkName(contract);
+            console.log("Check Name");
         }
-    },[contract]);
+    }, [contract,useName]);
     const checkName = async (contract) => {
         const name = await contract.getName();
+        console.log(name);
+        onFinalUsername(name);
         if(name === ""){
             setShownameInput(true);
         }else {
             setShownameInput(false);
             onFinalUsername(name);
+            //setIsMounted(false);
         }
     }
     const handleNameChange = (e) => {
         const value = e.target.value;
-        const maxValue = 50;
+        const maxValue = 16;
         if (value.length >= maxValue) {
             onError({localCode : "ERR002", localMessage :`Le nom ne peut pas dépasser ${maxValue} caractères !`});
         }
@@ -32,7 +37,9 @@ function SetName({ contract, onError, onFinalUsername}) {
             const transaction = await contract.setName(userName);
             await transaction.wait();
             setShownameInput(false);
+            console.log(userName);
             onFinalUsername(userName);
+            setUseName(false);
         } catch (err) {
             onError(err);
 
@@ -41,7 +48,7 @@ function SetName({ contract, onError, onFinalUsername}) {
     return (
         showNameInput && (
             <div className="setName">
-                <input type="text" value={userName} onChange={handleNameChange} placeholder="Entrez un nom d'utilisateur" maxLength={50}/>
+                <input type="text" value={userName} onChange={handleNameChange} placeholder="Entrez un nom d'utilisateur" maxLength={16}/>
                 <button onClick={setName}>Valider</button>
             </div>
         )
